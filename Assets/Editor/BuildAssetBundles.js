3fde672d6859b25aa1098e112e6d6fd2cde990b8
keijiro <keijiro@gmail.com>
2012-04-24 19:23:22 +0900

    Implemented AssetBundleTest.

--- Assets/Editor/BuildAssetBundles.js
+++ Assets/Editor/BuildAssetBundles.js
@@ -7,6 +7,12 @@ static function BuildAssetBundles() {
     var path = EditorUtility.SaveFolderPanel("Save asset bundles to directory", "", "");
     if (path.Length == 0) return;
     
+    BuildBundleOfTarget(BuildTarget.WebPlayer, Path.Combine(path, "textures-webplayer.unity3d"));
+    BuildBundleOfTarget(BuildTarget.iPhone, Path.Combine(path, "textures-ios.unity3d"));
+    BuildBundleOfTarget(BuildTarget.Android, Path.Combine(path, "textures-android.unity3d"));
+}
+
+private static function BuildBundleOfTarget(target : BuildTarget, path : String) {
     var objects = new Array();
     for (var filepath in Directory.GetFiles("Assets/Resources")) {
         if (filepath.EndsWith(".png")) {
@@ -17,6 +23,6 @@ static function BuildAssetBundles() {
     BuildPipeline.BuildAssetBundle(
         objects[0] as UnityEngine.Object,
         objects.ToBuiltin(UnityEngine.Object) as UnityEngine.Object[],
-        Path.Combine(path, "textures.unity3d")
+        path, 0, target
     );
 }
--- Assets/TestSuite/8.AssetBundleTest/AssetBundleTest.js
+++ Assets/TestSuite/8.AssetBundleTest/AssetBundleTest.js
@@ -1,9 +1,46 @@
 #pragma strict
 
-var url = "Asset bundle URL";
+#if UNITY_IPHONE
+private var url = "https://github.com/downloads/keijiro/unity-memory-example/textures-ios.unity3d";
+#elif UNITY_ANDROID
+private var url = "https://github.com/downloads/keijiro/unity-memory-example/textures-android.unity3d";
+#else
+private var url = "https://github.com/downloads/keijiro/unity-memory-example/textures-webplayer.unity3d";
+#endif
+
+static function ShowMemoryUsage() {
+    var textureMemory = 0;
+    for (var texture in Resources.FindObjectsOfTypeAll(Texture)) {
+        textureMemory += Profiler.GetRuntimeMemorySize(texture);
+    }
+    Debug.Log("HeapSize:" + Profiler.usedHeapSize + " / Textures:" + textureMemory);
+}
 
 function Start() {
-    var www = new WWW(url);
+    yield; yield; ShowMemoryUsage();
+
+    Debug.Log("LoadFromCacheOrDownload");
+    var www = new WWW.LoadFromCacheOrDownload(url, 0);
+//    var www = new WWW(url);
     yield www;
-    Debug.Log(www.assetBundle.mainAsset);
+
+    yield; yield; ShowMemoryUsage();
+
+    Debug.Log("LoadAll");
+    www.assetBundle.LoadAll(Texture2D);
+
+    yield; yield; ShowMemoryUsage();
+
+    Debug.Log("Unload(false)");
+    www.assetBundle.Unload(false);
+    www.Dispose();
+
+    yield; yield; ShowMemoryUsage();
+
+    Debug.Log("UnloadUnusedAssets");
+    yield Resources.UnloadUnusedAssets();
+
+    yield; yield; ShowMemoryUsage();
+
+    Application.LoadLevel("Termination");
 }
